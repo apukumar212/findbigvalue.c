9d7ca08e36fc4a2de3dd67dc180327907176bd3f
Apu kumar Giri <87590236+apukumar212@users.noreply.github.com>
2022-08-05 19:27:56 +0530

    Add files via upload

---
+++ findbigvalue.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int a,b;
+    printf("enter a & b value= ");
+    scanf("%d %d",&a,&b);
+    if(a>b) printf("%d is big value",a);
+    else printf("%d is big value",b);
+
+}
